c36bffa329d45abc8b43866db48db7338fb96df9
fady01 <116297802+fady01@users.noreply.github.com>
2025-12-19 21:45:03 +0200

    add a dto interface to the imageparser, speechtotext endpoints

---
+++ AI_ServiceProvider/DTOs/ImageParserHistoryRow.cs
@@ -0,0 +1,12 @@
+﻿namespace AI_ServiceProvider.DTOs
+{
+    public class ImageParserHistoryRow
+    {
+        public Guid InputId { set; get; }
+        public string ImageUrl {  set; get; }
+        public string? JsonSchema {  set; get; }
+        public DateTime UploadedAt {  set; get; }
+        public string? ParsedData {  set; get; }
+        public DateTime? GeneratedAt {  set; get; }
+    }
+}
